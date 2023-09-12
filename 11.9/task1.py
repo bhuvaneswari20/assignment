89d74a8c8b7e0b0ec3039a572647c31030743707
bhuvaneswari20 <144679271+bhuvaneswari20@users.noreply.github.com>
2023-09-12 12:11:20 +0530

    Add files via upload

---
+++ 11.9/task1.py
@@ -0,0 +1,12 @@
+Name=("bhuvaneswari raja")
+Address=("eswaramoorthipalayam namakkal-636202")
+Class=("Msc computer science")
+T=80
+E=80
+M=90
+Phy=90
+Che=80
+CS=95
+Total=T+E+M+Phy+Che+CS
+Avg=Total/6
+print("I'm",Name,"My address",Address,"i'm doing",Class,"My 12th mark:",T,E,M,Phy,Che,CS,"My total mark",Total,"Avg",Avg)
